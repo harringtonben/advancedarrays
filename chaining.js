9c694fa9b21115ea30831ac439cb6ce22a80abbc
ben harrington <harringtonben@icloud.com>
2017-08-15 20:43:21 -0500

    ok this is done for real this time

--- chaining.js
+++ chaining.js
@@ -2,27 +2,37 @@
 
 var numbers = [888, 8008, 7734, -79, 0, 2];
 
-// 1.  Sort the numbers in descending order (10, 9, 8, 7, etc).
-var sortedShit = numbers.sort(function(first,second){return first - second}).reverse();
+// // 1.  Sort the numbers in descending order (10, 9, 8, 7, etc).
+// var sortedShit = numbers.sort(function(first,second){return first - second}).reverse();
+
+// console.log(sortedShit);
+// // 2.  Remove any integers greater than 19.
+// var howBigAmI = numbers.filter(function(number) {
+// 	if (number < 19) {
+// 		return true;
+// 	}
+// });
+// console.log(howBigAmI);
+// // 3.  Multiply each remaining number by 1.5 and then subtract 1.
+
+// var gettingThere = howBigAmI.map(function(fruit) {
+// 	return (fruit * 1.5) -1;
+// })
+
+// console.log(gettingThere);
+// // 4.  Then output (either in the DOM or the console) the sum of all the resulting numbers.
+
+// var theHell = gettingThere.reduce(function(accumulation, currentValue) {
+// 	return accumulation + currentValue;
+// });
+// console.log(theHell);
+
+
+//here we go
+
+var sortedShit = numbers.sort(function(first,second){return second - first})
+				.filter(function(number) {if (number <= 19) {return true;}})
+				.map(function(fruit) {return (fruit * 1.5) -1;})
+				.reduce(function(accumulation, currentValue) {return accumulation + currentValue;});
 
 console.log(sortedShit);
-// 2.  Remove any integers greater than 19.
-var howBigAmI = numbers.filter(function(number) {
-	if (number > 19) {
-		return true;
-	}
-});
-console.log(howBigAmI);
-// 3.  Multiply each remaining number by 1.5 and then subtract 1.
-
-var gettingThere = howBigAmI.map(function(fruit) {
-	return (fruit * 1.5) -1;
-})
-
-console.log(gettingThere);
-// 4.  Then output (either in the DOM or the console) the sum of all the resulting numbers.
-
-var theHell = gettingThere.reduce(function(accumulation, currentValue) {
-	return accumulation + currentValue;
-});
-console.log(theHell);
